5de17c7e10f11214baeef781adc3239b3fd6f950
Alejandra-lugo <alugo130406@gmail.com>
2025-05-11 21:24:37 -0500

    se añate try catch sobre el main

--- src/co/edu/unbosque/controlador/Main.java
+++ src/co/edu/unbosque/controlador/Main.java
@@ -1,7 +1,5 @@
 package co.edu.unbosque.controlador;
 
-import java.util.ArrayList;
-
 import co.edu.unbosque.modelo.Jugador;
 import co.edu.unbosque.modelo.TorneoGrupos;
 import co.edu.unbosque.modelo.persistencia.DirectorioJugadoresDAO;
@@ -13,14 +11,14 @@ public class Main {
         DirectorioJugadoresDAO jugadoresDAO = new DirectorioJugadoresDAO("jugadores.bin");
         DirectorioTorneosDAO torneosDAO = new DirectorioTorneosDAO("torneos.bin");
 
-        Jugador jugador1 = new Jugador("pepito1", "dsdsd", "dasndjs", "shabdjas ", "dasdsa", "sapo");
-        Jugador jugador2 = new Jugador("pepito2", "dsdsd", "dasndjs", "shabdjas ", "dasdsa", "buho");
-        Jugador jugador3 = new Jugador("pepito356", "dsdsd", "dasndjs", "shabdjas ", "dasdsa", "goku");
-        Jugador jugador4 = new Jugador("pepito456", "dsdsd", "dasndjs", "shabdjas ", "dasdsa", "goku");
-        Jugador jugador5 = new Jugador("pepito5", "dsdsd", "dasndjs", "shabdjas ", "dasdsa", "buho");
-        Jugador jugador6 = new Jugador("pepito6", "dsdsd", "dasndjs", "shabdjas ", "dasdsa", "sapo");
-        Jugador jugador7 = new Jugador("pepito7", "dsdsd", "dasndjs", "shabdjas ", "dasdsa", "perro");
-        Jugador jugador8 = new Jugador("pepito8", "dsdsd", "dasndjs", "shabdjas ", "dasdsa", "perro");
+        Jugador jugador1 = new Jugador("pepito1", "dsdsd", "Colombia", "pepito1@mail.com", "Ajedrez", "sapo");
+        Jugador jugador2 = new Jugador("pepito2", "dsdsd", "Argentina", "pepito2@mail.com", "Fútbol", "buho");
+        Jugador jugador3 = new Jugador("pepito356", "dsdsd", "Chile", "pepito3@mail.com", "Tenis", "goku");
+        Jugador jugador4 = new Jugador("pepito456", "dsdsd", "Perú", "pepito4@mail.com", "Natación", "goku");
+        Jugador jugador5 = new Jugador("pepito5", "dsdsd", "Brasil", "pepito5@mail.com", "Ajedrez", "buho");
+        Jugador jugador6 = new Jugador("pepito6", "dsdsd", "México", "pepito6@mail.com", "Fútbol", "sapo");
+        Jugador jugador7 = new Jugador("pepito7", "dsdsd", "Ecuador", "pepito7@mail.com", "Tenis", "perro");
+        Jugador jugador8 = new Jugador("pepito8", "dsdsd", "Uruguay", "pepito8@mail.com", "Natación", "perro");
 
         jugadoresDAO.add(jugador1);
         jugadoresDAO.add(jugador2);
@@ -45,14 +43,18 @@ public class Main {
         torneo.getParticipantes().add(jugador7);
         torneo.getParticipantes().add(jugador8);
 
-        torneo.organizarEquipos();
-        torneo.generarCronograma();
-
-        torneo.mostrarCronograma();
-
-        torneosDAO.add(torneo);
-
-        System.out.println("Torneos registrados:");
-        System.out.println(torneosDAO.getAll());
+        try {
+            if (torneo.getParticipantes().size() < 8) {
+                throw new IllegalStateException("No hay suficientes jugadores para organizar el torneo.");
+            }
+            torneo.organizarEquipos();
+            torneo.generarCronograma();
+            torneo.mostrarCronograma();
+            torneosDAO.add(torneo);
+            System.out.println("Torneos registrados:");
+            System.out.println(torneosDAO.getAll());
+        } catch (IllegalStateException e) {
+            System.err.println("Error al organizar el torneo: " + e.getMessage());
+        }
     }
 }

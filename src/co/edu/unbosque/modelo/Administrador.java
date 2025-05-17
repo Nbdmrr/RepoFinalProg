3ee60d3dfcd549eddba28df1b22b3eb3b0271d67
unknown <danielestebanuzeta@gmail.com>
2025-05-17 18:37:12 -0500

    docu modelo

--- src/co/edu/unbosque/modelo/Administrador.java
+++ src/co/edu/unbosque/modelo/Administrador.java
@@ -1,23 +1,43 @@
 package co.edu.unbosque.modelo;
 
-public class Administrador extends Usuario{
+/**
+ * Clase que representa un administrador, que es un tipo de usuario.
+ * Contiene además el cargo que desempeña el administrador.
+ */
+public class Administrador extends Usuario {
 	
+	/** Cargo o posición del administrador */
 	private String cargo;
 	
-	
-	public Administrador(String usuario, String contraseña, String nacionalidad, String correo,String cargo) {
-	super(usuario,contraseña,nacionalidad,correo);
-     tipo = "Administrador";
-     this.setCargo(cargo);
-	
+	/**
+	 * Constructor de la clase Administrador.
+	 * 
+	 * @param usuario Nombre de usuario
+	 * @param contraseña Contraseña del usuario
+	 * @param nacionalidad Nacionalidad del administrador
+	 * @param correo Correo electrónico del administrador
+	 * @param cargo Cargo o posición que desempeña
+	 */
+	public Administrador(String usuario, String contraseña, String nacionalidad, String correo, String cargo) {
+		super(usuario, contraseña, nacionalidad, correo);
+		tipo = "Administrador";
+		this.setCargo(cargo);
 	}
 
-
+	/**
+	 * Obtiene el cargo del administrador.
+	 * 
+	 * @return cargo del administrador
+	 */
 	public String getCargo() {
 		return cargo;
 	}
 
-
+	/**
+	 * Establece el cargo del administrador.
+	 * 
+	 * @param cargo cargo a asignar al administrador
+	 */
 	public void setCargo(String cargo) {
 		this.cargo = cargo;
 	}
